a014457b8c763a755573d6971d98c2da84248bcb
Pham Van Hai <hai.pham@codecomplete.jp>
2019-04-04 17:48:28 +0700

    Adding match_data.json and DataManager

---
+++ iSeaGoals/iSeaGoals-Settings.swift
@@ -0,0 +1,15 @@
+//
+//  iSeaGoals-Settings.swift
+//  iSeaGoals
+//
+//  Created by Pham Van Hai on 4/4/19.
+//  Copyright © 2019 Pham Van Hai. All rights reserved.
+//
+
+import Foundation
+
+// Display Comments
+let kDebugLog = true
+
+let useLocalData = true
+let matchDataURL   = "http://yoururl.com/json/stations.json"
